085751228ae68a5ff07cd1fdc68515d807f19b1a
subodh1108 <subodh.kumarsahucse24@ssipmt.com>
2025-11-14 20:56:00 +0530

    Create array.c
    Fix syntax errors and improve sorting logic in array.c

---
+++ array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+int n,i,j,temp;
+  int a[10];
+  printf("enter nuber of element");
+  scanf("%d);
+    printf("enterEd integers\n",n);
+  for (i=0; i<n; i++)
+    {
+scanf("%d",&a[il);
+    }
+  for(i=1; i<n; i++)
+    {
+      temp=a[i];
+      j=i=1;
+      while (j>=0 && a[j]>temp)
+        {
+         a[j+1]=a[j];
+          j=i=1
+        }
+      a[j+1]=temp;
+}
+printf("sorted array is\n")
+for(i=0; i<n; i++)
+      {
+printf("%d\n",a[i]);
+      }
+    }
+
+
+
+
+}
